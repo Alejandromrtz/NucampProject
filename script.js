6918ed2cc385a8795cee275ed2bbe4ee8ac8c726
Alejandro Martinez <goatmix1@gmail.com>
2023-08-27 17:58:35 -0400

    intergrated weather api

--- script.js
+++ script.js
@@ -17,3 +17,45 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 }) 
+
+async function fetchWeather (){
+    try{
+        const apiKey = process.env.OPEN_WEATHER_API_KEY;
+        const city = 'Atlanta';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+        const response = await fetch(url);
+        console.log(response);
+    //checks if response was not successful; else it will throw an error
+        if (!response.ok) {
+        throw new Error(`error`);
+    }
+
+        const data = await response.json();
+        console.log(data);
+        
+    
+    displayWeather(data); // Call the displayWeather function
+        } catch (error) {
+            console.log(error);
+        }
+}
+
+
+function displayWeather(data){
+    const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+    const iconImage = document.createElement("img");
+    iconImage.src = iconUrl;
+    
+    const iconContainer = document.getElementById('weather');
+    iconContainer.appendChild(iconImage);
+
+    const temperature = document.getElementById('weather-temp');
+    temperature.textContent = `${data.main.temp} \u00B0`;
+
+    const description = document.getElementById('weather-description');
+    description.textContent = data.weather[0].description;
+};
+
+fetchWeather();
